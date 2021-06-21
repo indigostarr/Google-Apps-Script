14977cf61583c40ae6040f5cdc765c97b83c40b6
Indigo Starr <indigojstarr@gmail.com>
2021-06-21 12:22:54 -0600

    updating folder structure

--- Create PDFs/bonus_letters.js
+++ Create PDFs/bonus_letters.js
@@ -1,10 +1,13 @@
+// create a menu that displays when the sheet is opened and can trigger the  below formula
 function onOpen() {
   SpreadsheetApp.getUi()
     .createMenu("Generate Letters")
+    // second item is the formula
     .addItem("Generate Letters", "Bonus_Letters")
     .addToUi();
 }
 
+// create global variables that can be used
 var d = new Date();
 d.setDate(d.getDate() + 3);
 var DATE = d.toLocaleDateString("default", {
@@ -12,10 +15,18 @@ var DATE = d.toLocaleDateString("default", {
   day: "numeric",
   year: "numeric",
 });
+
+// this is useful to set status after script completes running for this line
 var Letter_Created = "Letter_Created";
-var folder = DriveApp.getFolderById("1x4011Fknq1dWi7GchHlSf2KadurlKRcT");
+var folder = DriveApp.getFolderById("google-folder-id-for-doc-template");
+
+// this will be the new folder for PDFs generated to live
 var payDayFolder = folder.createFolder(DATE).getId();
-var docFolder = DriveApp.getFolderById("1EGeK1f-DJxafkMzHIRuuLxKLldXcMy7I");
+
+// this is the parent folder where we will create a subfolder
+var docFolder = DriveApp.getFolderById("parent-folder-for-new-templates");
+
+// create a new folder that is created for the week/time of run
 var draftDoc = docFolder.createFolder("Bonus Letter PDFs" + DATE).getId();
 
 function Bonus_Letters() {
@@ -27,25 +38,13 @@ function Bonus_Letters() {
   var dataRange = sheet.getRange(startRow, 1, numRows, lastColumn); // Fetch the data range of the active sheet
   var data = dataRange.getValues(); // Fetch values for each row in the range
   var TODAY_DATE = Utilities.formatDate(new Date(), "PST", "MM/dd/yyyy");
-  //function getFriday(TODAY_DATE) {
-  // Copy provided date or use current date
-  //date = date? new Date(+date) : new Date();
-
-  // Get the day number
-  //var dayNum = date.getDay();
-
-  //Set date to next Monday
-  //date.setDate(date.getDate() + (dayNum? 5 - dayNum: 1));
-  //return date;
-  //}
-  // function createFolder() {
-  //   var folder = DriveApp.getFolderById('1x4011Fknq1dWi7GchHlSf2KadurlKRcT')
-  //   var payDayFolder = folder.createFolder(TODAY_DATE);
-  // }
 
   for (var i = 0; i < data.length; ++i) {
+    // create a variable for the row
     var row = data[i];
 
+    // this represents the data in each column row[i][col]
+    // apps script is 0 based so the first col is 0
     var KEYNAME = row[1];
     var EMPLOYEE = row[7];
     var EEID = row[0];
@@ -56,13 +55,12 @@ function Bonus_Letters() {
     var CURR = row[16];
     var STATUS = row[25];
     // var AMOUNT = Utilities.formatString("$%d,%02d%1.2f", MONEY/1000, MONEY%1000/10,MONEY%10)
+    // test outputs with
     //Logger.log(PLAN);
 
     if (PLAN == "Spot Bonus" && STATUS != "Letter_Created") {
       //file is the template file, and you get it by ID
-      var file = DriveApp.getFileById(
-        "1C8tzllCEUB8oyBjwrOFFXizy4IX7ZZuR7of7TtWN3-g"
-      );
+      var file = DriveApp.getFileById("google doc template file");
 
       //We can make a copy of the template, name it, and optionally tell it what folder to live in
       //file.makeCopy will return a Google Drive file object
@@ -80,7 +78,7 @@ function Bonus_Letters() {
       var style = {};
       style[DocumentApp.Attribute.FORMAT] = "Currency";
 
-      //Then we call all of our replaceText methods
+      //Then we call all of our replaceText methods and set from the data in the sheet
       body.replaceText("{{Date}}", TODAY_DATE);
       body.replaceText("{{Name}}", EMPLOYEE);
       body.replaceText("{{Amount}}", MONEY);
@@ -90,9 +88,14 @@ function Bonus_Letters() {
 
       //Lastly we save and close the document to persist our changes
       doc.saveAndClose();
+
+      // add a created text
       sheet.getRange(startRow + i, lastColumn).setValue("Letter_Created");
+
+      // create and save the PDF
       var blobFile = doc.getAs("application/pdf");
       var pdf = DriveApp.createFile(blobFile);
+      //
       var pdfFolder = DriveApp.getFolderById(payDayFolder);
       pdfFolder.addFile(pdf);
     }
